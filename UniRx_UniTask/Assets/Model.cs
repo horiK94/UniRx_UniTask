6da9c7232c9ae0d067ab9316ada29ef154f70418
horiK <hazemi94basic@gmail.com>
2020-12-20 02:40:11 +0900

    MVPの実装

---
+++ UniRx_UniTask/Assets/Model.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class Model : MonoBehaviour
+{
+    [SerializeField] private ReactiveProperty<int> num = new ReactiveProperty<int>(0);
+
+    public IReadOnlyReactiveProperty<int> Num => num;
+
+    public void UpdateNum(int _val)
+    {
+        num.Value = Mathf.Clamp(_val, 0, 100);
+    }
+}
---
+++ UniRx_UniTask/Assets/Preseter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UniRx;
+
+public class Preseter : MonoBehaviour
+{
+    //Modelへの参照
+    [SerializeField] private Model model;
+
+    //Viewへの参照
+    [SerializeField] private InputField inputField = null;
+    [SerializeField] private Slider slider = null;
+    [SerializeField] private Button upButton = null;
+    [SerializeField] private Button downButton = null;
+
+    private void Start()
+    {
+        // Model → View(生徒たち)
+        model.Num.Subscribe(_num =>
+        {
+            inputField.text = _num.ToString();
+            slider.value = _num;
+        }).AddTo(this);
+
+        //View → Model(先生たち)
+        inputField.OnValueChangedAsObservable()
+            .Select(_numString =>
+            {
+                bool isSucceed = int.TryParse(_numString, out int num);
+                return (isSucceed, num);
+            })
+            .Where(_val => _val.isSucceed)
+            .Subscribe(_val => model.UpdateNum(_val.num));
+
+        slider.OnValueChangedAsObservable()
+            .Subscribe(_value => model.UpdateNum((int) _value));
+
+        Observable.Merge(
+            upButton.OnClickAsObservable().Select(_ => +1),
+            downButton.OnClickAsObservable().Select(_ => -1)
+        ).Subscribe(_val => model.UpdateNum(_val + model.Num.Value));
+    }
+}
