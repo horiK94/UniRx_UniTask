1ffd342cd1e7e9c282b8b673577bd6b918c8829c
horiK <hazemi94basic@gmail.com>
2021-01-04 01:57:11 +0900

    Subjectのコード追加

---
+++ UniRx_UniTask/Assets/Session3/SubjectSample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class SubjectSample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+        var subject = new Subject<int>();
+
+        subject.OnNext(1);
+
+        //購読開始
+        subject.Subscribe(
+            x => Debug.Log("OnNext: " + x),
+            error => Debug.Log("OnError: " + error),
+            () => Debug.Log("OnCompleted"));
+
+        subject.OnNext(2);
+        subject.OnNext(3);
+
+        subject.OnCompleted();
+        subject.Dispose();
+    }
+}
