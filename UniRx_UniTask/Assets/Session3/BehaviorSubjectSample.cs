ffc3135af7f103b9d9d33f0350588567b95beff8
horiK <hazemi94basic@gmail.com>
2021-01-13 23:46:49 +0900

    ReplaySubjectまで勉強

---
+++ UniRx_UniTask/Assets/Session3/BehaviorSubjectSample.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class BehaviorSubjectSample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var behaviourSubject = new BehaviorSubject<int>(0);
+
+        behaviourSubject.OnNext(0);
+        behaviourSubject.OnNext(1);
+
+        behaviourSubject.Subscribe(
+            x => Debug.Log("OnNext: " + x),
+            error => Debug.Log("OnError: " + error),
+            () => Debug.Log("OnCompleted"));
+
+        Debug.Log("購読後");
+
+        behaviourSubject.OnNext(2);
+
+
+        behaviourSubject.Subscribe(
+            x => Debug.Log("OnNext2: " + x),
+            error => Debug.Log("OnError2: " + error),
+            () => Debug.Log("OnCompleted2"));
+
+        Debug.Log("現在の値" + behaviourSubject.Value);
+        behaviourSubject.OnNext(3);
+
+        Debug.Log("Completedを呼ぶ直前");
+
+        behaviourSubject.OnCompleted();
+    }
+}
---
+++ UniRx_UniTask/Assets/Session3/ReplaySubjectSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class ReplaySubjectSample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var subject = new ReplaySubject<int>(bufferSize: 3);
+
+        for (int i = 0; i < 10; i++)
+        {
+            subject.OnNext(i);
+        }
+
+        subject.OnCompleted();
+        //2回目のOnCompletedは通常通り呼ばれない
+        subject.OnCompleted();
+
+        //subject.OnError(new Exception("Error!"));
+
+        subject.Subscribe(
+            x => Debug.Log(x),
+            ex => Debug.Log("Error" + ex),
+            () => Debug.Log("OnCompleted"));
+    }
+}
