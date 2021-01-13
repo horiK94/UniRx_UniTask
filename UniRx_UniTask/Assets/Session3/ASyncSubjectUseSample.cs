f0cb260d7746b9fb19820cc7444cc9937ae6faab
horiK <hazemi94basic@gmail.com>
2021-01-14 01:37:08 +0900

    AsyncSubject実装

---
+++ UniRx_UniTask/Assets/Session3/ASyncSubjectUseSample.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class ASyncSubjectUseSample : MonoBehaviour
+{
+    [SerializeField] private AsyncSubjectSample asyncSubjectSample;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        asyncSubjectSample.PlayerTextureAsync
+                //AsyncSubjectはSubscrbe()のタイミングによらず、OnNext()を通知する
+            .Subscribe(SetMyTexture)
+            .AddTo(this);
+    }
+
+    void SetMyTexture(Texture newTexture)
+    {
+        var r = GetComponent<Renderer>();
+        r.sharedMaterial.mainTexture = newTexture;
+    }
+}
---
+++ UniRx_UniTask/Assets/Session3/AsyncSubjectSample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+
+//非同期のためのIObservable
+//AsyncObject<T>はOnCompleted()が呼ばれるまで結果を出力しない
+//OnCompleted()のときに最後にOnNext()されていたものを出力する
+public class AsyncSubjectSample : MonoBehaviour
+{
+    private AsyncSubject<Texture> playerTextureAsyncSubject = new AsyncSubject<Texture>();
+
+    public IObservable<Texture> PlayerTextureAsync => playerTextureAsyncSubject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(loadTexture());
+    }
+
+    private IEnumerator loadTexture()
+    {
+        var resource = Resources.LoadAsync<Texture>("Textures/player");
+
+        yield return resource;
+
+        //読み込み終了時に結果を通知
+        playerTextureAsyncSubject.OnNext(resource.asset as Texture);
+        playerTextureAsyncSubject.OnCompleted();
+    }
+}
